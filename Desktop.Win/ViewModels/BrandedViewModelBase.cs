8ee92492ea0c0292e328c31863e724ba100b3d1f
Jakub Potoczny <jakub.potoczny@nex-it.pl>
2021-10-14 17:24:28 +0200

    colors

--- Desktop.Win/ViewModels/BrandedViewModelBase.cs
+++ Desktop.Win/ViewModels/BrandedViewModelBase.cs
@@ -41,9 +41,9 @@ namespace Remotely.Desktop.Win.ViewModels
                     brandingInfo?.TitleBackgroundBlue ?? 0));
 
                 TitleForegroundColor = new SolidColorBrush(Color.FromRgb(
-                   brandingInfo?.TitleForegroundRed ?? 29,
-                   brandingInfo?.TitleForegroundGreen ?? 144,
-                   brandingInfo?.TitleForegroundBlue ?? 241));
+                   brandingInfo?.TitleForegroundRed ?? 0,
+                   brandingInfo?.TitleForegroundGreen ?? 160,
+                   brandingInfo?.TitleForegroundBlue ?? 227));
 
                 TitleButtonForegroundColor = new SolidColorBrush(Color.FromRgb(
                    brandingInfo?.ButtonForegroundRed ?? 255,
